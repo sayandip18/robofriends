8ac65d8fe35e5533f613fa190a5d3c40941bbd92
sayandip <sayandiph4@gmail.com>
2021-09-22 00:59:22 +0530

    making search bar work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return(
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={ robots } />
-        </div>
-    );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase());
+        })
+        return(
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={ this.onSearchChange }/>
+                <CardList robots={ filteredRobots } />
+            </div>
+        );
+    }
 }
 
 export default App;
